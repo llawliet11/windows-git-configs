f0a31c32b0cd0db090838f034d931ce904091886
LLAWLIET <llawliet11@users.noreply.github.com>
2020-09-14 17:29:34 +0700

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cat <<EOT>> /home/bitnami/.bash_profile
+PATH="/opt/bitnami/apps/wordpress/bin:/opt/bitnami/varnish/bin:/opt/bitnami/sqlite/bin:/opt/bitnami/php/bin:/opt/bitnami/mysql/bin:/opt/bitnami/letsencrypt/:/opt/bitnami/apache2/bin:/opt/bitnami/common/bin:$PATH"
+BITNAMI_ROOT="/opt/bitnami"
+export BITNAMI_ROOT
+
 alias set_permission="sh /home/bitnami/windows-git-configs/permissions.sh "
 alias get_app="cd /opt/bitnami/apps"
 EOT
