04716bdc9ac7b1fc910a3494daf98f4293e047e8
LLAWLIET <llawliet11@users.noreply.github.com>
2020-09-14 17:03:05 +0700

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,8 +1,8 @@
-#/bin/sh
+#/bin/bash
 
-cat <<EOT >> /home/bitnami/.bash_profile
-> alias set_permission="sh /home/bitnami/windows-git-configs/permissions.sh "
-> alias get_app="cd /opt/bitnami/apps"
-> EOT
+cat <<EOT>> /home/bitnami/.bash_profile
+alias set_permission="sh /home/bitnami/windows-git-configs/permissions.sh "
+alias get_app="cd /opt/bitnami/apps"
+EOT
 
-source /home/bitnami/.bash_profile
+. /home/bitnami/.bash_profile
